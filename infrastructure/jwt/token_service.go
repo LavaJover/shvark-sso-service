7fa66a6c6aadd02255d2e4d3fdc4a903c3c329ae
bodya <lavajover@gmail.com>
2025-04-30 00:59:08 +0300

    feat: add token infrastructure layer

---
+++ infrastructure/jwt/token_service.go
@@ -0,0 +1,61 @@
+package jwt
+
+import (
+	"time"
+	"errors"
+
+	"github.com/LavaJover/shvark-sso-service/internal/domain"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type jwtTokenService struct {
+	secretKey string
+	ttl time.Duration
+}
+
+func NewTokenService(secretKey string, ttl time.Duration) domain.TokenService {
+	return &jwtTokenService{
+		secretKey: secretKey,
+		ttl: ttl,
+	}
+}
+
+func (s *jwtTokenService) GenerateAccessToken(user *domain.User) (string, error){
+	claims := jwt.MapClaims{
+		"user_id": user.ID,
+		"exp":     time.Now().Add(s.ttl).Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(s.secretKey))
+}
+
+func (s *jwtTokenService) GenerateRefreshToken(user *domain.User) (string, error) {
+	// improve later...
+	return s.GenerateAccessToken(user)
+}
+
+func (s *jwtTokenService) ValidateAccessToken(tokenStr string) (string, error){
+	token, err := jwt.Parse(tokenStr, func(t *jwt.Token) (interface{}, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("unexpected signing method")
+		}
+		return []byte(s.secretKey), nil
+	})
+
+	if err != nil || !token.Valid {
+		return "", domain.ErrInvalidToken
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		return "", domain.ErrInvalidToken
+	}
+
+	userID, ok := claims["user_id"].(string)
+	if !ok {
+		return "", domain.ErrInvalidToken
+	}
+
+	return userID, nil
+}
