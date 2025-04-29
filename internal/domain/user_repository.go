b135c92880e767b0fd14b4b70722cee45b1e6df8
bodya <lavajover@gmail.com>
2025-04-30 00:37:34 +0300

    fix: rename field

--- internal/domain/user_repository.go
+++ internal/domain/user_repository.go
@@ -2,6 +2,6 @@ package domain
 
 type UserRepository interface {
 	Create(user *User) error
-	FindByLogin(email string) (*User, error)
+	FindByLogin(login string) (*User, error)
 	FindByID(id string) (*User, error)
 }
