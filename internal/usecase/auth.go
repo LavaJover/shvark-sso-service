2dfeb6aba1e6e3dffd6980775f0106f8aab26f44
bodya <lavajover@gmail.com>
2025-04-30 18:35:09 +0300

    fix AuthUseCase interface

--- internal/usecase/auth.go
+++ internal/usecase/auth.go
@@ -3,4 +3,5 @@ package usecase
 type AuthUseCase interface {
 	Register(login, username, password string) (userID string, err error)
 	Login(login, password string) (accessToken string, err error)
+	ValidateToken(token string) (userID string, err error)
 }
