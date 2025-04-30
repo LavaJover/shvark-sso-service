a2d91c7ca21d54b47c82184a09b02e9fdb9002c4
bodya <lavajover@gmail.com>
2025-04-30 18:26:47 +0300

    feat: add register abd login auth handlers

---
+++ internal/delivery/grpcapi/auth_handler.go
@@ -0,0 +1,45 @@
+package grpcapi
+
+import (
+	"context"
+
+	"github.com/LavaJover/shvark-sso-service/internal/usecase"
+	ssopb "github.com/LavaJover/shvark-sso-service/proto/gen"
+)
+
+type AuthHandler struct {
+	ssopb.UnimplementedSSOServiceServer
+	usecase.AuthUseCase
+}
+
+func (h *AuthHandler) Register(ctx context.Context, req *ssopb.RegisterRequest) (*ssopb.RegisterResponse, error) {
+	userID, err := h.AuthUseCase.Register(req.Login, req.Username, req.Password)
+	if err != nil{
+		return nil, err
+	}
+
+	return &ssopb.RegisterResponse{
+		UserId: userID,
+		Message: "Successfully created user",
+	}, nil
+}
+
+func (h *AuthHandler) Login(ctx context.Context, req *ssopb.LoginRequest) (*ssopb.LoginResponse, error) {
+	accessToken, err := h.AuthUseCase.Login(req.Login, req.Password)
+	if err != nil{
+		return nil, err
+	}
+
+	return &ssopb.LoginResponse{
+		AccessToken: accessToken,
+		RefreshToken: accessToken,
+	}, nil
+}
+
+func (h *AuthHandler) ValidateToken(ctx context.Context, req *ssopb.ValidateTokenRequest) (*ssopb.ValidateTokenResponse, error) {
+	
+}
+
+func (h *AuthHandler) GetUserByToken(ctx context.Context, req *ssopb.GetUserByTokenRequest) (*ssopb.GetUserByTokenResponse, error) {
+
+}
