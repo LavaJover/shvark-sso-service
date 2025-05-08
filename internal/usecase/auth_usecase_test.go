606451d9b330de9281987efd54b49140f5c5c7fd
bodya <lavajover@gmail.com>
2025-05-08 20:06:30 +0300

    feat: add TestAuthUsecase_Login_Success

--- internal/usecase/auth_usecase_test.go
+++ internal/usecase/auth_usecase_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/LavaJover/shvark-sso-service/internal/mocks"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func TestAuthUsecase_Register_Success(t *testing.T){
@@ -58,4 +59,40 @@ func TestAuthUsecase_Register_LoginExists(t *testing.T) {
 
 	assert.ErrorIs(t, err, domain.ErrInvalidLogin)
 	assert.Empty(t, userID)
+
+	mockUserRepo.AssertExpectations(t)
+}
+
+func TestAuthUsecase_Login_Success(t *testing.T) {
+	mockUserRepo := new(mocks.UserRepository)
+	mockToken := new(mocks.TokenService)
+
+	id := "some-id"
+	login := "boris"
+	username := "britva"
+	password := "secure"
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	assert.NoError(t, err)
+
+	user := &domain.User{ID: id, Login: login, Username: username, Password: string(hashedPassword)}
+
+	// Поведение: логин пользователя найден
+	mockUserRepo.On("FindByLogin", login).
+		Return(user, nil)
+
+	// Поведение: access-token пользователя успешно сгенерирован
+	expectedToken := "some-token"
+	mockToken.On("GenerateAccessToken", user).
+		Return(expectedToken, nil)
+
+	// Запуск auth usecase
+	authUC := NewAuthUseCase(mockUserRepo, mockToken)
+	actualToken, err := authUC.Login(login, password)
+	
+	assert.NoError(t, err)
+	assert.Equal(t, expectedToken, actualToken)
+
+	mockUserRepo.AssertExpectations(t)
+	mockToken.AssertExpectations(t)
 }
