1d2ec23cb67c623989207986b6511fe8a51fefc4
bodya <lavajover@gmail.com>
2025-05-16 19:38:23 +0300

    refactor

--- internal/interceptor/logger.go
+++ internal/interceptor/logger.go
@@ -15,14 +15,15 @@ func NewLoggerInterceptor(logger *zap.Logger) grpc.UnaryServerInterceptor {
 		info *grpc.UnaryServerInfo,
 		handler grpc.UnaryHandler,
 	) (resp any, err error) {
+		// before calling business logic
 		start := time.Now()
 		logger.Info("Incoming gRPC request",
 			zap.String("method", info.FullMethod),
 			zap.Any("request", req),
 		)
-
+		// calling business logic
 		resp, err = handler(ctx, req)
-
+		// afetr calling business logic
 		logger.Info("Completed gRPC request",
 			zap.String("method", info.FullMethod),
 			zap.Duration("duration", time.Since(start)),
